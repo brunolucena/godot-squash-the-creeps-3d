86e2c6c40e5699d12ef958d384d62f3c0890ea7b
Bruno Lucena <bruno.lucenac@gmail.com>
2022-06-14 13:08:40 -0300

    Score, Retry and Music

--- Main.gd
+++ Main.gd
@@ -5,6 +5,7 @@ export (PackedScene) var mob_scene
 
 func _ready():
 	randomize()
+	$UserInterface/Retry.hide()
 
 func _on_MobTimer_timeout():
 	# Create a new instance of the Mob scene
@@ -19,8 +20,17 @@ func _on_MobTimer_timeout():
 	var player_position = $Player.transform.origin
 	mob.initialize(mob_spawn_location.translation, player_position)
 	
+	# We connect the mob to the score label to update the score upon squashing one
+	mob.connect("squashed", $UserInterface/ScoreLabel, "_on_Mob_squashed")
+	
 	add_child(mob)
 
 
 func _on_Player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene
+		get_tree().reload_current_scene()
